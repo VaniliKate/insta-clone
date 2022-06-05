5f23d2b87bd41d923661803e5a2fc6294e64a626
Vanili <katevanili98@gmail.com>
2022-06-06 01:06:46 +0300

    New file additions

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/infinite/Music/instagram-clone-mainapp (1)/instagram-clone-1/venv"
+set -gx VIRTUAL_ENV "/home/vanili/Documents/moringa-school-projects/Django/insta-clone/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
